1dbe5e8d3f52429c5961f3fab038a9872a297935
Asus TUF <killingeveryonerc@gmail.com>
2025-06-06 15:52:55 +0300

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <div className="hero-section">
-            <h1>Find the best work</h1>
+            <h1>Find the best work TEST VERSION</h1>
             <p>Vacancies from all across the world</p>
         </div>
     );
